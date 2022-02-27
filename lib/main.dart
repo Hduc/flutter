87c079891f0eb4c56cf2235932e0fcbd1a265776
nghoangducit@gmail.com <nghoangducit@gmail.com>
2022-02-27 11:50:20 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
+import 'constants.dart';
 import 'pages/name_your_price.dart';
 
 void main() {
@@ -10,10 +12,20 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kBgColor,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: TextButton.styleFrom(backgroundColor: kPrimaryColor),
+        ),
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: kBodyTextColor),
+          bodyText2: TextStyle(color: kBodyTextColor),
+          headline5: TextStyle(color: kDarkBlackColor),
+        ),
       ),
       home: const NameYourPricePage(),
     );
